1d4354494d6b671295d28fa61695b66d9117b7d2
Jonas Nilsson <nilssoncjonas@gmail.com>
2022-11-16 19:07:29 +0100

    menu nav

---
+++ dist/js/script.js
@@ -0,0 +1,23 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+} 
+
+function myFunction(x) {
+  x.classList.toggle("change");
+} 
+
+/* Open */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+} 
