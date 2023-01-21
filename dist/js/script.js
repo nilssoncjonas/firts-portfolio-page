de660c8cde80b77e7d00009f6d141b557d4aef38
Jonas Nilsson <nilssoncjonas@gmail.com>
2023-01-21 16:44:34 +0100

    completed accordion

--- dist/js/script.js
+++ dist/js/script.js
@@ -29,3 +29,16 @@ menuItems.forEach(
   ) 
 
 // accordion at my project section
+const items = document.querySelectorAll('.accordion button')
+
+items.forEach(item => item.addEventListener('click', toggleAccordion))
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute("aria-expanded")
+  for(let item of items) {
+    item.setAttribute("aria-expanded", false)
+  }
+  if (itemToggle === 'false') {
+    this.setAttribute("aria-expanded", true)
+  }
+}
